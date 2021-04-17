36b4a7a0299b120ab7f7b41e918ea6eefe16e860
Stefan Michel <funjobie@hotmail.com>
2021-04-18 00:52:44 +0200

    launch game through steam to avoid warning message

--- WholeEarthProject/FactorioWEP/start_wep_client.py
+++ WholeEarthProject/FactorioWEP/start_wep_client.py
@@ -5,9 +5,13 @@ from pathlib import Path
 
 def start_wep_client():
 
-  path_to_factorio = wep_settings.get_path_to_factorio()
-
-  ls_output=subprocess.run(path_to_factorio + " --mp-connect 127.0.0.1:4798")
+  if wep_settings.get_launch_factorio_through_steam():
+    # 427520 is the app id of factorio (as can be seen from the url of https://store.steampowered.com/app/427520/Factorio/ )
+    path_to_steam = wep_settings.get_path_to_steam()
+    ls_output=subprocess.run(path_to_steam + " -applaunch 427520 --mp-connect 127.0.0.1:4798")
+  else:
+    path_to_factorio = wep_settings.get_path_to_factorio()
+    ls_output=subprocess.run(path_to_factorio + " --mp-connect 127.0.0.1:4798")
 
 if __name__ == "__main__":
   start_wep_client()
--- WholeEarthProject/FactorioWEP/wep_settings.py
+++ WholeEarthProject/FactorioWEP/wep_settings.py
@@ -5,3 +5,17 @@ def get_path_to_factorio():
     return path
   else:
     raise FileNotFoundError("could not find factorio executable. please set path in wep_settings.py")
+
+# if this is true, then the game client is launched from within steam. if "False" the client is opened directly.
+# recommended to be "True" as directly launching it produces a warning message from steam which the player needs to click before it actually launches it.
+# see discussion in https://steamcommunity.com/discussions/forum/1/458604254465948374/
+def get_launch_factorio_through_steam():
+  return True
+    
+# If the above setting is true, the path to steam is needed which will be used to start the factorio client.
+def get_path_to_steam():
+  path = "D:/Steam/steam.exe"
+  if os.path.isfile(path):
+    return path
+  else:
+    raise FileNotFoundError("could not find steam executable. please set path in wep_settings.py")
